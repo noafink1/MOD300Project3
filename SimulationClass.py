640fa576deb1a98460689844741851788e840dba
Andreas Werner <267177@uis.no>
2023-10-25 12:28:35 +0200

    everything works

--- SimulationClass.py
+++ SimulationClass.py
@@ -17,15 +17,16 @@ class Simulation():
         self.infection_probability_ = q
         self.HUMAN = 0
         self.ZOMBIE = 1
-        self.timestep = []
-        self.no_humans = []
-        self.no_zombies = []
+        self.no_humans = np.empty(0, dtype=int)    
+        self.no_zombies = np.empty(0, dtype=int)  
         self.STATE = np.repeat(self.HUMAN, self.N_)
         self.Walkers = np.random.randint(0, [self.nx_, self.ny_], size=(self.N_, 2))
         self.Old_Walkers = np.copy(self.Walkers)
         
     def init_zombies(self, n):
+        self.IO_ = n
         self.STATE[0:n] = self.ZOMBIE
+
     
     def check_illegal_move(self):
         wrong_place_x = np.logical_or(self.Walkers[:,0] < 0, self.Walkers[:,0] > self.nx_-1)
@@ -58,11 +59,11 @@ class Simulation():
         plt.figure(figsize=(6,6))
         plt.xlim(-1, self.nx_+1)
         plt.ylim(-1, self.ny_+1)
-        # xticks = np.arange(0, self.nx_+2, 1)
-        # yticks = np.arange(0, self.ny_+2, 1)
-        # plt.xticks(xticks)
-        # plt.yticks(yticks)
-        plt.scatter(H[:,0], H[:,1], color='blue', s=120)
+        xticks = np.arange(0, self.nx_+2, 1)
+        yticks = np.arange(0, self.ny_+2, 1)
+        plt.xticks(xticks)
+        plt.yticks(yticks)
+        plt.scatter(H[:,0], H[:,1], color='blue', s=60)
         plt.scatter(Z[:,0], Z[:,1], color='red', s=60)
         plt.plot([0, self.ny_], [0,0], linestyle='dashed', color='black')
         plt.plot([0, self.ny_], [self.nx_, self.nx_], linestyle='dashed', color='black')
@@ -90,9 +91,16 @@ class Simulation():
                 plt.figure(figsize=(10,10))
                 plt.xlim(-1, self.nx_+1)
                 plt.ylim(-1, self.ny_+1)
+<<<<<<< HEAD
                 plt.grid()
                 plt.xticks([])
                 plt.yticks([])
+=======
+                xticks = np.arange(0, self.nx_+2, 1)
+                yticks = np.arange(0, self.ny_+2, 1)
+                plt.xticks(xticks)
+                plt.yticks(yticks)
+>>>>>>> 02a0109483cef380e67f1c67a4b95f5ced57ea23
                 plt.scatter(H[:,0], H[:,1], color='blue', s=60)
                 plt.scatter(Z[:,0], Z[:,1], color='red', s=60)
                 plt.plot([0, self.ny_], [0,0], linestyle='dashed', color='black')
@@ -108,31 +116,36 @@ class Simulation():
                 plt.close()
                 if np.sum(self.STATE == 0) == 0:
                     break
+<<<<<<< HEAD
         frames[0].save(f"{name}.gif", save_all=True, append_images=frames[1:], optimize=True, duration=500, loop=0)
         plt.close()
         self.reset()
 
+=======
+        
+        frames[0].save(name, save_all=True, append_images=frames[1:], optimize=True, duration=500, loop=0)
+        plt.close()
+        self.reset()
+>>>>>>> 02a0109483cef380e67f1c67a4b95f5ced57ea23
 
     def calculate_no_humans_and_zombies(self):
         return np.sum(self.STATE == 0), np.sum(self.STATE == 1)
     
     def reset(self):
         self.STATE = np.repeat(self.HUMAN, self.N_)
-        self.STATE[0] = self.ZOMBIE
+        self.init_zombies(self.IO_)
         self.Walkers = np.random.randint(0, [self.nx_, self.ny_], size=(self.N_, 2))
         self.Old_Walkers = np.copy(self.Walkers)
-        self.timestep = []
-        self.no_humans = []
-        self.no_zombies = []
+        self.no_humans = np.empty(0, dtype=int)
+        self.no_zombies = np.empty(0, dtype=int)
 
     def run_simulation(self, n):
         for i in range(n):
             self.move_walkers()
             if np.all(self.check_collision != 0):
                 self.set_zombie()
-            self.no_humans.append(self.calculate_no_humans_and_zombies()[0])
-            self.no_zombies.append(self.calculate_no_humans_and_zombies()[1])
-            self.timestep.append(i)
+            self.no_humans=np.append(self.no_humans,self.calculate_no_humans_and_zombies()[0])
+            self.no_zombies = np.append(self.no_zombies,self.calculate_no_humans_and_zombies()[1])
             # self.plot()
 
 
