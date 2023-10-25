6d978b0f58bd60aa115d4032331353f85cdc28df
noafink1 <92817137+noafink1@users.noreply.github.com>
2023-10-25 16:35:40 +0200

    25. okt - DEAD ZOMBIES are moving

--- SimulationClass.py
+++ SimulationClass.py
@@ -9,7 +9,8 @@ class Simulation():
                 no_init_infected=1,
                 nx=50,
                 ny=50,
-                q=0.9):
+                q=0.9,
+                p_death = 0.0):
         self.N_ = population_size
         self.IO_ = no_init_infected
         self.nx_ = nx
@@ -17,8 +18,11 @@ class Simulation():
         self.infection_probability_ = q
         self.HUMAN = 0
         self.ZOMBIE = 1
+        self.DEAD_ZOMBIE = -1
+        self.p_death = p_death
         self.no_humans = np.empty(0, dtype=int)    
         self.no_zombies = np.empty(0, dtype=int)  
+        self.beta = np.empty(0, dtype=float)
         self.STATE = np.repeat(self.HUMAN, self.N_)
         self.Walkers = np.random.randint(0, [self.nx_, self.ny_], size=(self.N_, 2))
         self.Old_Walkers = np.copy(self.Walkers)
@@ -35,14 +39,15 @@ class Simulation():
     
     def move_walkers(self):
         u = np.array([[0,1],[0,-1],[1,0],[-1,0]])
-        dir = np.random.randint(0,4,size=self.N_)
-        self.Walkers = self.Walkers + u[dir]
+        alive_mask = self.STATE != self.DEAD_ZOMBIE
+        dir = np.random.randint(0,4,size=np.sum(alive_mask))
+        self.Walkers[alive_mask] = self.Walkers[alive_mask] + u[dir]
         index = self.check_illegal_move()
         self.Walkers[index] = self.Old_Walkers[index]
         self.Old_Walkers = np.copy(self.Walkers)
 
     def check_collision(self):
-        zombie_array = self.Walkers[self.STATE == 1]
+        zombie_array = self.Walkers[self.STATE == self.ZOMBIE]
         match_mask = np.all(zombie_array[:, None, :] == self.Walkers, axis=-1)
         zombie_collision_with_zombies = np.any(match_mask, axis=0)
         return np.logical_xor(zombie_collision_with_zombies, self.STATE)
@@ -51,11 +56,18 @@ class Simulation():
         random = np.around(np.random.uniform(0.0, 1.0, self.N_), 2)
         condition1 = random <= self.infection_probability_
         condition2 = self.check_collision() == 1
-        self.STATE = np.where(condition1 & condition2, 1, self.STATE)
+        self.STATE = np.where(condition1 & condition2, self.ZOMBIE, self.STATE)
+
+    def check_if_zombies_die(self):
+        if self.p_death != 0:
+            random = np.around(np.random.uniform(0.0, 1.0, self.N_), 2)
+            condition1 = random <= self.p_death
+            condition2 = self.STATE == self.ZOMBIE
+            self.STATE = np.where(condition1 & condition2, self.DEAD_ZOMBIE, self.STATE)
 
     def plot(self):
-        H = self.Walkers[self.STATE == 0]
-        Z = self.Walkers[self.STATE == 1]
+        H = self.Walkers[self.STATE == self.HUMAN]
+        Z = self.Walkers[self.STATE == self.ZOMBIE]
         plt.figure(figsize=(6,6))
         plt.xlim(-1, self.nx_+1)
         plt.ylim(-1, self.ny_+1)
@@ -84,18 +96,20 @@ class Simulation():
             self.move_walkers()
             if np.all(self.check_collision != 0):
                 self.set_zombie()
-
+            self.check_if_zombies_die()
             if i % 5 == 0:             
-                H = self.Walkers[self.STATE == 0]
-                Z = self.Walkers[self.STATE == 1]
+                H = self.Walkers[self.STATE == self.HUMAN]
+                Z = self.Walkers[self.STATE == self.ZOMBIE]
+                D = self.Walkers[self.STATE == self.DEAD_ZOMBIE]
                 plt.figure(figsize=(10,10))
                 plt.xlim(-1, self.nx_+1)
                 plt.ylim(-1, self.ny_+1)
                 plt.grid()
                 plt.xticks([])
                 plt.yticks([])
-                plt.scatter(H[:,0], H[:,1], color='blue', s=60)
-                plt.scatter(Z[:,0], Z[:,1], color='red', s=60)
+                #plt.scatter(H[:,0], H[:,1], color='blue', s=60)
+                #plt.scatter(Z[:,0], Z[:,1], color='red', s=60)
+                plt.scatter(D[:,0], D[:,1], color='black', s=60)
                 plt.plot([0, self.ny_], [0,0], linestyle='dashed', color='black')
                 plt.plot([0, self.ny_], [self.nx_, self.nx_], linestyle='dashed', color='black')
                 plt.plot([0,0], [0,self.ny_], linestyle='dashed', color='black')
@@ -107,7 +121,7 @@ class Simulation():
                 img = Image.open(buf)
                 frames.append(img)
                 plt.close()
-                if np.sum(self.STATE == 0) == 0:
+                if np.sum(self.STATE == self.HUMAN) == 0:
                     break
         frames[0].save(f"{name}.gif", save_all=True, append_images=frames[1:], optimize=True, duration=500, loop=0)
         plt.close()
@@ -115,7 +129,15 @@ class Simulation():
 
 
     def calculate_no_humans_and_zombies(self):
-        return np.sum(self.STATE == 0), np.sum(self.STATE == 1)
+        return np.sum(self.STATE == self.HUMAN), np.sum(self.STATE == self.ZOMBIE)
+    
+    def calculate_beta(self):
+        curr_humans, curr_zombies = self.calculate_no_humans_and_zombies()
+
+        if curr_humans == 0 or curr_zombies == 0:
+            return 0
+
+        return -np.round(((curr_humans - self.no_humans[-1])*self.N_)/(curr_humans*curr_zombies), 3)
     
     def reset(self):
         self.STATE = np.repeat(self.HUMAN, self.N_)
@@ -124,17 +146,17 @@ class Simulation():
         self.Old_Walkers = np.copy(self.Walkers)
         self.no_humans = np.empty(0, dtype=int)
         self.no_zombies = np.empty(0, dtype=int)
+        self.beta = np.empty(0, dtype=float)
 
     def run_simulation(self, n):
         for i in range(n):
             self.move_walkers()
             if np.all(self.check_collision != 0):
                 self.set_zombie()
+            if i >= 1:
+                self.beta = np.append(self.beta,self.calculate_beta())
             self.no_humans=np.append(self.no_humans,self.calculate_no_humans_and_zombies()[0])
             self.no_zombies = np.append(self.no_zombies,self.calculate_no_humans_and_zombies()[1])
-            # self.plot()
-
-
-
+            self.check_if_zombies_die()
 
         
